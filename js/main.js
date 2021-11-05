d3827ba3addc30230b46c3716cd1690f7d08e9a5
Aexer <77164402+ycelik0@users.noreply.github.com>
2021-11-05 21:35:10 +0100

    added a bit of jf for darkmode button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
+// DARK MODE
+const theme = document.querySelector("#theme-toggle");
+const toggle = document.querySelector(".light-mode-active");
+
+if (theme) {
+  theme.addEventListener("click", function () {
+    const theNav = document.querySelector(".navbar");
+    const theNavBrand = document.querySelector(".navbar-brand");
+    const theNavLinks = document.querySelector(".nav-link");
+    const theMain = document.querySelector(".themain");
+    const theFrontpage = document.querySelector(".frontpage-image");
+    const theArrowDown = document.querySelector(".bxs-down-arrow");
+    const theImage = document.querySelector(".img-self-self");
+    const theExperience = document.querySelector(".experience-tab-text-text");
+    const theContact = document.querySelector(".contact-title-title");
+    const theFooter = document.querySelector(".thefooter");
+    theNav.classList.toggle("light-mode-active");
+    theNavLinks.classList.toggle("light-mode-active-text");
+    theNavBrand.classList.toggle("light-mode-active-text");
+    theMain.classList.toggle("light-mode-active");
+    theFrontpage.classList.toggle("light-mode-active");
+    theArrowDown.classList.toggle("light-mode-active");
+    theImage.classList.toggle("light-mode-active-border");
+    theExperience.classList.toggle("light-mode-active-text");
+    theContact.classList.toggle("light-mode-active-contact-text");
+    theFooter.classList.toggle("light-mode-active-footer");
+  })
+}
+toggle.onclick = function () {
+  toggle.classList.toggle('light-mode-active')
+}
+
 // COPY PASTE EMAIL
 const button = document.querySelector(".copytoClipboard");
 if (button) {
   button.addEventListener("click", function () {
-    let mail = "yc5693410@gmail.com";
+    const mail = "yc5693410@gmail.com";
     navigator.clipboard.writeText(mail);
   })
 }
